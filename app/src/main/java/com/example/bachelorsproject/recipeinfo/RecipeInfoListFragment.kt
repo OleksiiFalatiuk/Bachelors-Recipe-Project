e88e0ae1c4b205d5acf88badc2ee851a2567ea7f
Ynikum <alexfalatyuk98@gmail.com>
2022-02-09 11:24:18 +0200

    by click on image of some dish, opens second fragment with detail information of it

--- app/src/main/java/com/example/bachelorsproject/recipeinfo/RecipeInfoListFragment.kt
+++ app/src/main/java/com/example/bachelorsproject/recipeinfo/RecipeInfoListFragment.kt
@@ -8,13 +8,25 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.bachelorsproject.R
 import com.example.bachelorsproject.model.RecipeInfo
+import com.example.bachelorsproject.provider.RecipeProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class RecipeInfoListFragment: Fragment() {
+
+    private val scopeRecipeInfo = CoroutineScope(Dispatchers.Main + SupervisorJob())
+
+    private val viewRecipeInfoModel: RecipeInfoViewModel by viewModels{
+        RecipeInfoViewModelFactory((requireActivity() as RecipeProvider).provideRecipe())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +44,22 @@ class RecipeInfoListFragment: Fragment() {
 
             this.adapter = adapter
         }
+
+        viewRecipeInfoModel.loadRecipeInfoAndComponents(recipeId)
+
+        scopeRecipeInfo.launch {
+            viewRecipeInfoModel.getRecipeInfoLiveData.observe(viewLifecycleOwner, { recipeInfo ->
+                if (recipeInfo != null){
+                    bindUI(view,recipeInfo)
+                }
+            })
+        }
+    }
+
+    private fun bindUI(view: View, recipe: RecipeInfo){
+        updateRecipeInfo(recipe)
+        val adapter = view.findViewById<RecyclerView>(R.id.recycler_ingredients).adapter as RecipeInfoAdapter
+        adapter.submitList(recipe.components)
     }
 
     private fun updateRecipeInfo(recipe: RecipeInfo){
